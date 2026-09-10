306f1c7190037c911a32b0b17365ad67a4059d9f
Sawyer025 <17001664@alumnos.utleon.edu.mx>
2026-09-09 18:44:29 -0600

    ts final 09/09/2026

--- src/temasTS/05-desestructuracion-funciones.ts
+++ src/temasTS/05-desestructuracion-funciones.ts
@@ -1,4 +1,4 @@
-interface Producto{
+export interface Producto{
     desc:string,
     precio:number,
 
@@ -30,7 +30,7 @@ const isv=calcularISV(articulos);
 console.log("ISV: ",isv);
 
 
-function calcularISV2(productos:Producto[]):[number,number]{
+export function calcularISV2(productos:Producto[]):[number,number]{
     let total=0;
     for(const producto of productos){
         total+=producto.precio;
--- src/temasTS/06-import-export.ts
+++ src/temasTS/06-import-export.ts
@@ -17,4 +17,4 @@ const carrito:Producto[]=[
 
 const [total,isv]=calcularISV2(carrito);
 console.log("Total carrito: ",total);
-console.log("ISV carrito: ",isv)
+console.log("ISV carrito: ",isv);
